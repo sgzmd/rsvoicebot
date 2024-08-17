694e15252cb68dcc64c903636690604513af47f9
Roman Kirillov <roman@kirillov.cc>
2024-08-17 17:53:26 +0100

    Adding some clarification comments

--- bot/src/audio_conversion.rs
+++ bot/src/audio_conversion.rs
@@ -1,6 +1,8 @@
 pub mod audio_conversion {
     use std::error::Error;
 
+    // AudioConverter trait with a single method to convert audio data to WAV format.
+    // It returns bytes of the converted WAV file (pcm,_s16le, 44100 Hz), not the samples.
     pub trait AudioConverter {
         fn convert_audio_to_wav(&self, input_data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>>;
     }
